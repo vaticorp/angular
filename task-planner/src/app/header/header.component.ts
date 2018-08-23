0b60e26482238fc5a7dc63ed1291c7756696cc0e
vaticorp <vaticorp@mail.ru>
2018-08-23 23:40:13 +0400

    2. Компонент. Модуль. [#61901]

---
+++ task-planner/src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html?v=${new Date().getTime()}',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
