42a5171cbec17571fc82193de736cfaf49a3d0d3
vaticorp <vaticorp@mail.ru>
2018-09-06 21:02:11 +0400

    3. Пайпы [#64254][#69415] - исправление

--- task-planner/src/app/shared/pipes/my-title-case.pipe.ts
+++ task-planner/src/app/shared/pipes/my-title-case.pipe.ts
@@ -7,18 +7,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class MyTitleCasePipe implements PipeTransform {
 
   transform(value: string) {
-/*    var result = '';
-    var pattern = new RegExp("\b\p{Lu}(\p{Ll}*)\b");
-    console.log(pattern.exec(value));
-/!*    value.replace(/b/p{Lu}(/p{Ll}*)/b);
-                  /[^ -:0-9]+/g*!/
-    /!*array.forEach(s => {
+    let array = value.split(' ');
+    var result = '';
+    array.forEach(s => {
       result += s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase() + ' ';
-    })*!/
-
-    var result = value.replace("\b\p{Lu}(\p{Ll}*)\b","");
-    /!*value.split(pattern);*!/*/
-
-    return value.split(/\s+/).map(word => word[0].toUpperCase() + word.substring(1)).join(' ');
+    })
+    return result;
   }
 }
